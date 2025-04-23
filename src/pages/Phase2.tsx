049e7cdc683b36bac79fdc4cf6466064f482ddb7
Kuo Jeong Hui <77497975+AndreiBautista@users.noreply.github.com>
2025-04-23 08:42:54 -0400

    Update Phase2.tsx

--- src/pages/Phase2.tsx
+++ src/pages/Phase2.tsx
@@ -259,7 +259,7 @@ const Phase2 = () => {
                   <tr className="border-b">
                     <th className="text-left py-3">Item</th>
                     <th className="text-right py-3">Cost</th>
-                    <th className="text-left py-3 pl-2">Notes</th>
+                    <th className="text-left py-3 pl-5">Notes</th>
                   </tr>
                 </thead>
                 <tbody>
